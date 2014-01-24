8a9019428ca14976f43e54ef6fd516b3730ec10f
Al Wold <alwold@alwold.com>
2014-01-24 14:50:39 -0700

    update term

--- spec/uwest_schedule_scraper_spec.rb
+++ spec/uwest_schedule_scraper_spec.rb
@@ -16,21 +16,21 @@ describe UwestScheduleScraper, '#get_class_info' do
   it "open class shows open status" do
     # open = get_class :open
     # scraper.get_class_status(open.term_code, open.abbrev, open.course_number, open.class_number).should eq(:open)
-    scraper.get_class_status("162", "ENGL101", "").should eq(:open)
+    scraper.get_class_status("163", "ANTHR301", "").should eq(:open)
   end
   it "closed class shows closed status" do
-    scraper.get_class_status("162", "REL636", "").should eq(:closed)
+    scraper.get_class_status("163", "REL636", "").should eq(:closed)
   end
   it "class info can be loaded" do
-    info = scraper.get_class_info('162', 'ENGL101', '')
+    info = scraper.get_class_info('163', 'ENGL101', '1')
     info.name.should eq("English Composition")
     # info.schedule.should eq("TR 10:00 AM - 11:50 AM")
   end
   it "returns nil for non-existent class" do
-    scraper.get_class_status('162', 'REL633', '').should eq(nil)
-    scraper.get_class_info('162', 'REL633', '').should eq(nil)
+    scraper.get_class_status('163', 'REL633', '').should eq(nil)
+    scraper.get_class_info('163', 'REL633', '').should eq(nil)
   end
   it "blows up with not enough params" do
-    expect { scraper.get_class_info("162", "ENGL101") }.to raise_error(ArgumentError)
+    expect { scraper.get_class_info("163", "ENGL101") }.to raise_error(ArgumentError)
   end
 end
